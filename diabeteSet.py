88249ca371edd44e9a5b09e6ee3c8626c079108e
Stella <stylianiterzi@gmail.com>
2025-03-21 03:18:43 +0200

    NF

---
+++ diabeteSet.py
@@ -0,0 +1,17 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+data = pd.read_csv('diabetes.csv')
+print("Missing values in each column:")
+print(data.isnull().sum())
+print("\nDataset Info:")
+print(data.info())
+plt.figure(figsize=(12, 8))
+correlation_matrix = data.corr().round(2)
+sns.heatmap(data=correlation_matrix, annot=True,cmap="YlGnBu")
+
+#sns.heatmap(data.isnull(), cbar=False, cmap='viridis')
+#plt.title("Missing Values Heatmap")
+plt.show()
